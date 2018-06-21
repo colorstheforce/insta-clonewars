81efd0c726cb148a3a44e07f490b37cc33ef25d7
jack ogina <jackogina60@gmail.com>
2018-06-20 23:17:01 -0400

    added models to the admin panel
    added userprofile and post models to the admin model

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts.models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('url','user','time_created','likes')
+admin.site.register(Post, PostAdmin)
--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from userprofile.models import UserProfileData
+
+# Register your models here.
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user','avatar')
+admin.site.register(UserProfileData, ProfileAdmin)
