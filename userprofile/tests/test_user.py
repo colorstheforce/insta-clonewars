3d8e8eb2cca22aa05b83f0e7ab1c631f203747ed
jack ogina <jackogina60@gmail.com>
2018-06-21 00:27:22 -0400

    rewrote TestCase methods for the test_user test

--- userprofile/tests/test_user.py
+++ userprofile/tests/test_user.py
@@ -7,23 +7,23 @@ class TestUser(TestCase):
 
 
     def test_instance(self):
-        self.assertEqual(isinstance(self.testuser,User),True)
+        self.assertIsInstance(self.testuser,User)
 
     def test_save_user(self):
-        self.assertEqual(self.testuser in User.objects.all(),False)
+        self.assertFalse(self.testuser in User.objects.all())
         self.testuser.save()
-        self.assertEqual(self.testuser in User.objects.all(),True)
+        self.assertTrue(self.testuser in User.objects.all())
         self.testuser.delete()
 
     def test_delete_user(self):
         self.testuser.save()
-        self.assertEqual(self.testuser in User.objects.all(),True)
+        self.assertTrue(self.testuser in User.objects.all())
         self.testuser.delete()
-        self.assertEqual(self.testuser in User.objects.all(),False)
+        self.assertFalse(self.testuser in User.objects.all())
 
     def test_pass_check(self):
         self.testuser.set_password('password')
-        self.assertEqual(self.testuser.check_password('password'),True)
+        self.assertTrue(self.testuser.check_password('password'))
         
     
 
